3f13df318b6b3054d3dce68ad0b054ac2dab157e
kathatbat <katoria1234@icloud.com>
2023-10-03 21:10:07 -0500

    script copy

--- script.js
+++ script.js
@@ -1,95 +1,92 @@
-const defaultRecipes = [
-    { name: 'Spaghetti Carbonara', description: 'A classic Italian pasta dish with eggs, cheese, and pancetta.' },
-    { name: 'Chicken Alfredo', description: 'Creamy pasta dish with grilled chicken and Alfredo sauce.' },
-    { name: 'Vegetable Stir-Fry', description: 'Stir-fried vegetables with tofu in a savory sauce.' },
-    { name: 'Homemade Pizza', description: 'Delicious homemade pizza with your favorite toppings.' },
-    // Add more recipes here...
-];
+var edamamAppId = "cd4e5584";
+var edamamAppKey = "73d84eab9b2b08ba19b3a16596fc6954";
 
-var recipes = JSON.parse(localStorage.getItem('recipes')) || defaultRecipes;
+var searchInput = document.getElementById('search-input');
+var recipeList = document.getElementById('recipe-list');
+var searchButton = document.getElementById('search-button');
 
-const searchInput = document.getElementById('search-input');
-const recipeList = document.getElementById('recipe-list');
+searchButton.addEventListener('click', () => {
+    var searchTerm = searchInput.value.trim();
+    $.get(`https://api.edamam.com/search?app_id=${edamamAppId}&app_key=${edamamAppKey}&q=${searchTerm}`, function (data) {
+        displayRecipes(data);
+    });
+});
 
-// Function to display recipes based on search input
-function displayRecipes(searchTerm) {
-    recipeList.innerHTML = ''; // Clear previous results
+function displayRecipes(recipeData) {
+    recipeList.innerHTML = '';
 
-    const filteredRecipes = recipes.filter(recipe =>
-        recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    if (filteredRecipes.length === 0) {
+    var recipeCount = recipeData.hits.length
+    if (recipeCount === 0) {
         recipeList.innerHTML = '<p>No recipes found.</p>';
         return;
     }
+    
+    // grab each recipe object from data
+    var recipes = recipeData.hits.map(function(recipe) {
+        return recipe.recipe;
+    });
 
-    filteredRecipes.forEach(recipe => {
-        const recipeItem = document.createElement('div');
+    recipes.forEach(recipe => {
+        var recipeItem = document.createElement('div');
         recipeItem.classList.add('recipe-item');
         recipeItem.innerHTML = `
-            <h2>${recipe.name}</h2>
-            <p>${recipe.description}</p>
+            <h2>
+                ${recipe.label}
+                <button>Favorite *</button>
+            </h2>
+            <img src="${recipe.image}" alt="${recipe.label}" class="recipe-image">
+            <ul>
+                <li>
+                    ${recipe.ingredientLines.join("</li><li>")}
+                </li>
+            </ul>
         `;
         recipeList.appendChild(recipeItem);
     });
-
-    const favButton = document.createElement('button');
-    favButton.textContent = 'Favorite me!';
-    document.body.appendChild(favButton)
-
-    favButton.addEventListener('click', () => {
-    alert('Favorite button clicked!');
-})
-
-// Event listener for input changes
-searchInput.addEventListener('input', () => {
-    const searchTerm = searchInput.value.trim();
-    displayRecipes(searchTerm);
-});
-
-// Initial display of all recipes
-displayRecipes('');
+}
 
 
 
 
 
-const formEl = document.querySelector("#search-form");
+// var formEl = document.querySelector("#search-form");
 
 
 gapi.load('client', initClient);
 
 function initClient() {
     gapi.client.init({
-        apiKey: 'put our own api key to use',
+        apiKey: 'AIzaSyB0CEWjg3JYSVDh742-xOUpieEeiZE1UMA',
         discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"],
     }).then(function () {
-        // API is ready
         console.log('api ready');
     });
 }
 
-function handleSearch(event) {
-    event.preventDefault();
+// function handleSearch1(event) {
+//     event.preventDefault();
 
-    var searchValue = document.querySelector('#search-input').value;
+//     var searchValue = document.querySelector('#search-input').value;
 
-    searchVideos(searchValue);
-}
+//     searchVideos(searchValue);
+// }
 
-function searchVideos(query) {
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: query,
-        maxResults: 10  // You can adjust this number based on your preference
-    });
+// function searchVideos(query) {
+//     var request = gapi.client.youtube.search.list({
+//         part: 'snippet',
+//         q: query,
+//         maxResults: 10  // You can adjust this number based on your preference
+//     });
 
-    request.execute(function (response) {
-        // Handle the response data here
-        console.log(response);
-    });
-}
+//     request.execute(function (response) {
+//         // Handle the response data here
+//         console.log(response);
+//     });
+// }
+ 
+//  const favButton = document.createElement('button');
+// favButton.textContent = 'Favorite me!';
+// document.body.appendChild(favButton)
 
-formEl.addEventListener("submit", handleSearch)
-}
+// favButton.addEventListener('click', () => {
+// alert('Favorite button clicked!');
